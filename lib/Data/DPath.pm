333084dfceca780f5783d32da8c9c323cc5dba00
Steffen Schwigon <ss5@bascha.(none)>
2008-11-20 16:22:57 +0100

    - Moose'ified trivial start

--- lib/Data/DPath.pm
+++ lib/Data/DPath.pm
@@ -3,137 +3,40 @@ package Data::DPath;
 use warnings;
 use strict;
 
+use Mouse;
+
 our $VERSION = '0.01';
 
-# ----- SYNOPSIS ----------------------------------------
+use parent 'Exporter';
+our @EXPORT_OK = qw(dpath);
+our %EXPORT_TAGS = (
+                    all => [qw(dpath)]
+                   );
 
-use Data::DPath 'dpath';
+use Data::DPath::Path;
+use Data::DPath::Context;
 
-my $dpath = dpath('//AAA/*/CCC');
-my $data  = {
-             AAA  => { BBB   => { CCC  => [ qw/ XXX YYY ZZZ / ] } },
-             some => { where => { else => {
-                                           AAA => { BBB => { CCC => 'affe' } },
-                                          } } },
-             strange_keys => { 'DD DD' => { 'EE/E' => { CCC => 'zomtec' } } },
-            };
+has path => ( isa => "Str", is  => "rw" );
 
-# classic calls
-@resultlist = $dpath->match($data);
-@resultlist = Data::DPath->find($data, '//AAA/*/CCC');
-# ( ['XXX', 'YYY', 'ZZZ'], 'affe' )
+sub dpath($)
+{
+        my ($path) = @_;
+        return Data::DPath::Path->new(path => $path);
+}
 
-# via Perl 5.10 smart matching
+sub get_context($)
+{
+        my ($path) = @_;
+        return Data::DPath::Context->new(path => $path);
+}
 
-@resultlist = $data ~~ $dpath;
-@resultlist = $data ~~ dpath('//AAA/*/CCC');
-@resultlist = $data ~~ dpath '//AAA/*/CCC';
-# ( ['XXX', 'YYY', 'ZZZ'], 'affe' )
+sub match
+{
+        my ($data, $path) = @_;
+        my $p = new Data::DPath::Path(path => $path);
+        return $p->match($data);
+}
 
-# filter
-@resultlist = $data ~~ dpath '//AAA/*/CCC[$#_ == 2]'; # array with 3 elements (last index is 2)
-@resultlist = $data ~~ dpath '//AAA/*/CCC[@_  == 3]'; # array with 3 elements
-# same as
-@resultlist = $data ~~ dpath '//AAA/*/CCC/[$#_ == 2]';
-@resultlist = $data ~~ dpath '//AAA/*/CCC/[@_  == 3]';
-# ( ['XXX', 'YYY', 'ZZZ'] )
-
-
-@resultlist = $data ~~ dpath '//AAA/*/CCC/*';
-# ( 'XXX', 'YYY', 'ZZZ', 'affe' )
-
-@resultlist = $data ~~ dpath '/AAA/*/CCC/*';
-# ( 'XXX', 'YYY', 'ZZZ' )
-
-@resultlist = $data ~~ dpath '/AAA/*/CCC/* | /some/where/else/AAA/BBB/CCC';
-# ( 'XXX', 'YYY', 'ZZZ', 'affe' )
-
-@resultlist = $data ~~ dpath '/AAA/*/CCC/*[2]';
-# ( 'ZZZ' )
-
-@resultlist = $data ~~ dpath '//AAA/*/CCC/*[2]';
-# ( 'ZZZ' )
-
-@resultlist = $data ~~ dpath '/strange_keys/DD DD/EE\/E/CCC';
-@resultlist = $data ~~ dpath '/strange_keys/"DD DD"/"EE/E"/CCC';
-#@resultlist = $data ~~ dpathx '.', '.strange_keys.DD DD.EE/E.CCC'; # allow different step separator, rivals parent step ".."
-# ( 'zomtec' )
-
-# context objects for incremental searches
-$context = Data::DPath->get_context($data, '//AAA/*/CCC');
-$context->find_all();
-# ( ['XXX', 'YYY', 'ZZZ'], 'affe' )
-
-# dpath inside context, same as: Data::DPath->find($data, '//AAA/*/CCC/*[2]')
-$context->find('/*[2]');
-$context ~~ dpath '/*[2]';
-#$context ~~ dpathx '.', '.*[2]';
-# ( 'ZZZ' )
-
-# ----------------------------------------
-
-my $data2 = [
-             'UUU',
-             'VVV',
-             'WWW',
-             { AAA  => { BBB   => { CCC  => [ qw/ XXX YYY ZZZ / ] } } },
-            ];
-
-@resultlist = $data2 ~~ dpath '/*';
-# ( 'UUU', 'VVV', 'WWW', { AAA  => { BBB   => { CCC  => [ qw/ XXX YYY ZZZ / ] } } } )
-
-@resultlist = $data2 ~~ dpath '/*[2]';
-# ( 'WWW' )
-
-@resultlist = $data2 ~~ dpath '//*[2]';
-# ( 'WWW', 'ZZZ' )
-
-@resultlist = $data2 ~~ dpath '/*[3]';
-# ( { AAA  => { BBB   => { CCC  => [ qw/ XXX YYY ZZZ / ] } } } )
-
-# ----------------------------------------
-
-my $data3  = {
-              AAA  => bless( { BBB => { CCC  => [ qw/ XXX YYY ZZZ / ] } }, "Foo::Bar"), # blessed BBB
-              some => { where => { else => {
-                                            AAA => { BBB => { CCC => 'affe' } }, # plain BBB
-                                           } } },
-              neighbourhoods => [
-                                 { 'DDD' => { EEE => { F1 => 'affe',
-                                                       F2 => 'tiger',
-                                                       F3 => 'fink',
-                                                       F4 => 'star',
-                                                     },
-                                              FFF => 'interesting value' }
-                                 },
-                                 { 'DDD' => { EEE => { F1 => 'bla',
-                                                       F2 => 'bli',
-                                                       F3 => 'blu',
-                                                       F4 => 'blo',
-                                                     },
-                                              FFF => 'boring value' }
-                                 },
-                                ],
-             };
-
-@resultlist = $data3 ~~ dpath '//AAA/BBB[ref($_) eq "Foo::Bar"]/CCC';
-# ( ['XXX', 'YYY', 'ZZZ'] )
-
-# parent step
-@resultlist = $data3 ~~ dpath '//DDD/EEE/F1[$_ eq "affe"]/../FFF'; # the DDD/FFF where the neighbor DDD/EEE/F1 == "affe"
-# ( 'interesting value' )
-
-# filter expressions can directly or indirectly follow a step (without or with slash), so this is the same
-@resultlist = $data3 ~~ dpath '//DDD/EEE/F1/[$_ eq "affe"]/../FFF';
-# ( 'interesting value' )
-
-# same via direct access
-@resultlist = $data3 ~~ dpath '/neighbourhoods/*[0]/DDD/FFF';
-# ( 'interesting value' )
-
-
-
-# ----- END SYNOPSIS ----------------------------------------
 
 # ::Tree
 #   ::Node   (references to current expressions)
