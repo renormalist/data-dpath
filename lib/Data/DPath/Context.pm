ee947452e83c84dc4adcd75e96fcbbf340d45565
Steffen Schwigon <ss5@bascha.(none)>
2008-11-20 16:22:40 +0100

    - new ::Path and ::Context classes

---
+++ lib/Data/DPath/Context.pm
@@ -0,0 +1,20 @@
+package Data::DPath::Context;
+
+use strict;
+use warnings;
+
+use 5.010;
+
+use Mouse;
+
+sub find_all
+{
+        return ('affe', 'zomtec');
+}
+
+sub search
+{
+        return new Data::DPath::Context;
+}
+
+1;
---
+++ lib/Data/DPath/Path.pm
@@ -0,0 +1,15 @@
+package Data::DPath::Path;
+
+use strict;
+use warnings;
+
+use 5.010;
+
+use Mouse;
+
+sub match
+{
+        return ('affe', 'zomtec');
+}
+
+1;
